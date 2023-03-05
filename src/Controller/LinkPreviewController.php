4da79860dcdcf2bd9461cc911f12d6807d188edf
Jean-Yves Curien <jy.curien@laposte.net>
2023-03-05 10:12:22 +0100

    add missing phpdoc

--- src/Controller/LinkPreviewController.php
+++ src/Controller/LinkPreviewController.php
@@ -5,9 +5,19 @@ namespace App\Controller;
 use App\Service\PreviewService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class LinkPreviewController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     #[Route('/preview/{url}', name: 'preview', requirements: ['url' => '.+'])]
     public function preview(PreviewService $previewService, string $url)
     {
